899159c131c2caaaf5d6097c48049c99d7e3ff6d
random-words <sivko.anton.ua@gmail.com>
2024-04-18 01:19:32 +0300

    in fact, textAnalyzer - done

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,12 +1,118 @@
 const text = require("./text");
 
 class Analyzer {
-  constructor() {}
+  moodKeywords = {
+    negative: [],
+    neutral: [],
+    positive: [],
+  };
+
+  // вони шось не дуже універсальні, вот ці вот два метода...
+  filterText(text) {
+    const textInLowerCase = text.toLowerCase();
+    const textWithoutSymbols = textInLowerCase
+      .replaceAll(/[^a-zA-Z0-9']/g, " ")
+      .split(" ");
+    const result = textWithoutSymbols
+      .toSorted((a, b) => a.localeCompare(b))
+      .filter((word) => word.trim());
+
+    return result;
+  }
+  getWordsQuantity(text) {
+    const wordsCounter = {};
+    for (const word of text) {
+      if (!wordsCounter.hasOwnProperty(word)) {
+        wordsCounter[word] = 0;
+      }
+
+      wordsCounter[word] += 1;
+    }
+
+    return wordsCounter;
+  }
 
   getMostUsedWords(text) {
-    const words = text.split(" ").toSorted((a, b) => a.localeCompare(b));
-    console.log(words);
+    const filteredText = this.filterText(text);
+    const wordsCounter = this.getWordsQuantity(filteredText);
+
+    const topTenWords = [];
+    for (let i = 0; i < 10; i += 1) {
+      let maxQuantity = 0;
+      let maxQuantityWord = "";
+      for (const word in wordsCounter) {
+        if (wordsCounter[word] > maxQuantity) {
+          maxQuantity = wordsCounter[word];
+          maxQuantityWord = word;
+        }
+      }
+      topTenWords.push([maxQuantityWord, maxQuantity]);
+      wordsCounter[maxQuantityWord] = null;
+    }
+
+    return topTenWords;
+  }
+  getUniqueWords(text) {
+    const filteredText = this.filterText(text);
+    const wordsCounter = this.getWordsQuantity(filteredText);
+
+    const uniqueWords = [];
+    for (const word in wordsCounter) {
+      if (wordsCounter[word] < 2) {
+        uniqueWords.push(word);
+      }
+    }
+
+    return uniqueWords;
+  }
+
+  getAverageTextLength(text) {
+    const sentences = text
+      .split(/(?<=[.!?])(?=\s+|\n+(?=\p{Lu}))/gu)
+      .map((sentence) => sentence.trim());
+    let overallTextLength = 0;
+    for (const sentence of sentences) {
+      overallTextLength += sentence.length;
+    }
+    return `Overall words in single sentence in text: ${(
+      overallTextLength / sentences.length
+    ).toFixed(2)} words`;
+  }
+  getMinWordsQuantityInSentence(text) {
+    const sentences = text
+      .split(/(?<=[.!?])(?=\s+|\n+(?=\p{Lu}))/gu)
+      .map((sentence) => sentence.trim());
+
+    let minSentenceLength = Infinity;
+    for (const sentence of sentences) {
+      if (sentence.length < minSentenceLength) {
+        minSentenceLength = sentence.length;
+      }
+    }
+    return minSentenceLength;
+  }
+  getMaxWordsQuantityInSentence(text) {
+    const sentences = text
+      .split(/(?<=[.!?])(?=\s+|\n+(?=\p{Lu}))/gu)
+      .map((sentence) => sentence.trim());
+
+    let maxSentenceLength = 0;
+    for (const sentence of sentences) {
+      if (sentence.length > maxSentenceLength) {
+        maxSentenceLength = sentence.length;
+      }
+    }
+    return maxSentenceLength;
+  }
+
+  // в душі не шарю, як це зробити
+  determineMoodSentence(text) {
+    const sentences = text
+      .split(/(?<=[.!?])(?=\s+|\n+(?=\p{Lu}))/gu)
+      .map((sentence) => sentence.trim());
   }
 }
 
-Analyzer.getMostUsedWords(text);
+const textAnalyzer = new Analyzer();
+
+module.exports = textAnalyzer;
--- src/main.js
+++ src/main.js
@@ -1,40 +1,8 @@
+const textAnalyzer = require("./analyzer");
 const text = require("./text");
 
-class Analyzer {
-  constructor() {}
-
-  getMostUsedWords(text) {
-    const symbols = [",", ".", "!", "?", ":", ";", "/"];
-    const sortedFilteredText = text
-      .split(" ")
-      .toSorted((a, b) => a.localeCompare(b))
-      .filter((word) => word.trim());
-    const textInLowerCase = sortedFilteredText
-      .join(" ")
-      .toLowerCase()
-      .split(" ");
-    const result = textInLowerCase.map((word) => {
-      let cleanWord = "";
-      for (const char of word) {
-        if (!symbols.includes(char)) {
-          cleanWord += char;
-        }
-      }
-      return cleanWord;
-    });
-
-    const wordsCounter = {};
-    for (const word of result) {
-      if (!wordsCounter.hasOwnProperty(word)) {
-        wordsCounter[word] = 0;
-      }
-
-      wordsCounter[word] += 1;
-    }
-    console.log(wordsCounter);
-  }
-}
-
-const textAnalyzer = new Analyzer();
-
-textAnalyzer.getMostUsedWords(text);
+console.log(textAnalyzer.getMostUsedWords(text));
+console.log(textAnalyzer.getUniqueWords(text));
+console.log(textAnalyzer.getAverageTextLength(text));
+console.log(textAnalyzer.getMinWordsQuantityInSentence(text));
+console.log(textAnalyzer.getMaxWordsQuantityInSentence(text));
